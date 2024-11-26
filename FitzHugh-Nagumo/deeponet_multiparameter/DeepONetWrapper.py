aebdab43ac4c3c5fd31c2ff072b854a1eaef0fbd
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-11-26 10:32:29 -0600

    First deeponet timeseries results are bad.

--- FitzHugh-Nagumo/deeponet_multiparameter/DeepONetWrapper.py
+++ FitzHugh-Nagumo/deeponet_multiparameter/DeepONetWrapper.py
@@ -2,9 +2,9 @@ import os
 import pickle
 from functools import partial
 
-import jax.numpy as jnp
+import jax.numpy as np
 import jax.nn as jnn
-from jax import jit, vmap
+from jax import vmap
 
 
 # The DeepONet class for the Fitzhugh-Nagumo equation represenst a map from one state (u, v, eps) to another (u, v)
@@ -16,8 +16,7 @@ class DeepONetWrapper:
         self.best_branch_params = self.best_params[0]
         self.best_trunk_params = self.best_params[1]
 
-        self.p = 300 # Number of output neurons in both the branch and trunk net outputs.
-        self.nx = 401
+        self.p = 300
 
     def load_model_params(self, resultdir, filename='model_params.pkl'):
         load_path = os.path.join(resultdir, filename)
@@ -28,35 +27,32 @@ class DeepONetWrapper:
     def BranchNet(self, x):
         def single_forward(params, x):
             for w, b in params:
-                outputs = jnp.dot(x, w) + b
+                outputs = np.dot(x, w) + b
                 x = jnn.silu(outputs)
             return outputs
 
         return vmap(partial(single_forward, self.best_branch_params))(x)
 
     def TrunkNet(self, x):
-        inputs = jnp.array(x)
         for w, b in self.best_trunk_params:
-            outputs = jnp.dot(x, w) + b
+            outputs = np.dot(x, w) + b
             x = jnn.silu(outputs)
         return outputs
 
-    @jit
-    def DeepONet(self, branch_inputs, trunk_inputs):
+    def DeepONet(self, u, v, eps, grid):
         # Branch_inputs and trunk_inputs are 1D arrays in my implementation. 
         # I convert then to (1, .) and (., 1) shape here, as well as rescale
-        branch_inputs = branch_inputs[jnp.newaxis,:]
-        trunk_inputs = trunk_inputs[:,jnp.newaxis]
-        branch_inputs[0, 0:200] = branch_inputs[0, 0:200] / 21 # Reschaling u(x) ?
-        branch_inputs[0,200:400] = branch_inputs[0, 200:400] / 1.e4 # Reschaling v(x) ? 
+
+        branch_inputs = np.concatenate((u / 21, v / 1.e4, np.array([eps])))[np.newaxis,:]
+        trunk_inputs = grid[:,np.newaxis] 
 
         b_out = self.BranchNet(branch_inputs)
         t_out = self.TrunkNet(trunk_inputs)
-        u = jnp.einsum('ik, lk -> il', b_out[:,0:self.p], t_out[:,0:self.p])
-        v = jnp.einsum('ik, lk -> il', b_out[:,self.p:2*self.p], t_out[:,self.p:2*self.p])
+        u = np.einsum('ik, lk -> il', b_out[:,0:self.p], t_out[:,0:self.p])
+        v = np.einsum('ik, lk -> il', b_out[:,self.p:2*self.p], t_out[:,self.p:2*self.p])
 
-        u = 21.0 * u
-        v = 1.e4 * v
+        u = 21.0 * u[0,:]
+        v = 1.e4 * v[0,:]
         return u, v
     
     def __call__(self, branch_inputs, trunk_inputs):
--- FitzHugh-Nagumo/deeponet_multiparameter/testDeepONet.py
+++ FitzHugh-Nagumo/deeponet_multiparameter/testDeepONet.py
@@ -12,60 +12,33 @@ def plotFitzHughNagumoSolution():
     # Model parameters
     L = 20.0
     N = 200
-    dx = L / N
-    a0 = -0.03
-    a1 = 2.0
-    delta = 4.0
-    eps = 0.1 # 0.01 originally for the spatio-temporal oscillations
-    params = {'delta': delta, 'eps': eps, 'a0': a0, 'a1': a1}
+    eps = 0.1
 
     # Initial condition
-    x_array = np.linspace(0.0, L, N)
-    u0 = sigmoid(x_array, 6.0, -1, 1.0, 2.0)
-    v0 = sigmoid(x_array, 10, 0.0, 2.0, 0.1)
-    u = np.copy(u0)
-    v = np.copy(v0)
+    x_array = np.linspace(0.0, L, N) / L
+    u = sigmoid(x_array, 6.0, -1, 1.0, 2.0)
+    v = sigmoid(x_array, 10, 0.0, 2.0, 0.1)
+
+    deeponet = DeepONetWrapper()
 
     # Timestepping
-    dt = 1.e-3
-    report_dt = 10*dt
-    T = 450.0
-    u_solution = np.zeros((int(T / report_dt)+1, u.size))
-    v_solution = np.zeros((int(T / report_dt)+1, u.size))
-    u_solution[0,:] = u0
-    v_solution[0,:] = v0
+    dt = 1.0
+    T = 10.0
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(x_array, u, label='u(x, t=0.0)')
+    ax2.plot(x_array, v, label='v(x, t=0.0)')
     for n in range(int(T / dt)):
-        u, v = fhn_euler(u, v, dx, dt, params)
+        u, v = deeponet.DeepONet(u, v, eps, x_array)
+        print(u.shape)
 
-        if n > 0 and n % 10 == 0:
-            u_solution[n // 10, :] = u
-            v_solution[n // 10, :] = v
-
-    # Store the time-evolution steady - state
-    np.save(directory + 'euler_evolution_T=' + str(T) + '.npy', np.vstack((x_array, u, v)))
+        ax1.plot(x_array, u, label='u(x, t='+str(n*dt)+')')
+        ax2.plot(x_array, v, label='v(x, t='+str(n*dt)+')')
 
     # Plotting the final result
-    x_plot_array = np.linspace(0.0, T, u_solution.shape[1]+1)
-    t_plot_array = np.linspace(0.0, T, u_solution.shape[0]+1)
-    plt.plot(x_array, u, label='u(x, t=450)')
-    plt.plot(x_array, v, label='v(x, t=450)')
-    plt.legend()
-
-    X, Y = np.meshgrid(x_plot_array, t_plot_array)
-    v_solution = sigmoid(v_solution, y_scale=2.0, y_center=-1.0, x_scale=0.05)
-    u_max = np.max(u_solution)
-    u_min = np.min(u_solution)
-    v_max = np.max(v_solution)
-    v_min = np.min(v_solution)
-    print('psi', lg.norm(psi(np.concatenate((u,v)), 0.1, dx, dt, params)))
-    plt.figure()
-    plt.pcolor(X, Y, u_solution, cmap='viridis', vmin=min(u_min, v_min), vmax=max(u_max, v_max))
-    plt.xlabel(r'$x$')
-    plt.ylabel(r'$t$')
-    plt.title(r'$u(x, t)$')
-    plt.figure()
-    plt.pcolor(X, Y, v_solution, cmap='viridis', vmin=min(u_min, v_min), vmax=max(u_max, v_max))
-    plt.xlabel(r'$x$')
-    plt.ylabel(r'$t$')
-    plt.title(r'$v(x, t)$')
+    ax1.set_title(r'$u(x,t)$')
+    ax2.set_title(r'$v(x,t)$')
+    ax1.legend()
     plt.show()
+
+if __name__ == '__main__':
+    plotFitzHughNagumoSolution()
