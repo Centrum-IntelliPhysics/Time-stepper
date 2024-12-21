e7ffab800fffcd7cab8f741daff0723444d2782d
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-21 12:32:25 +0100

    Fixing long overdue bugs in the data generation script.

--- FitzHugh-Nagumo/data_generation_scripts/GenerateSingleEpsTrainingData.py
+++ FitzHugh-Nagumo/data_generation_scripts/GenerateSingleEpsTrainingData.py
@@ -4,6 +4,7 @@ sys.path.append('../')
 import numpy as np
 import numpy.linalg as lg
 import numpy.random as rd
+import matplotlib.pyplot as plt
 
 from EulerTimestepper import fhn_euler_timestepper, calculateSteadyState, psi
 
@@ -12,20 +13,15 @@ def sigmoid(x, x_center=0.0, y_center=0.0, x_scale=1.0, y_scale=1.0):
     return y_scale / (1.0 + np.exp(-(x  - x_center)/x_scale)) + y_center
 
 def timeSimulation(u0, v0, dx, dt, T, dT, params):
-    n_entries = int(T / dT) + 1
-    solution_slices = np.zeros((n_entries, len(u0) + len(v0)))
+    n_timesteps = int(T / dT)
+    solution_slices = np.zeros((n_timesteps+1, len(u0) + len(v0)))
     solution_slices[0,:] = np.concatenate((u0, v0))
 
     u = np.copy(u0)
     v = np.copy(v0)
-    report_n = int(dT / dt)
-    for i in range(n_entries-1):
+    for i in range(n_timesteps):
         u, v = fhn_euler_timestepper(u, v, dx, dt, dT, params, verbose=False)
-
-        if i % report_n == 0:
-            index = i // report_n
-            solution_slices[index+1,:] = np.concatenate((u, v))
-
+        solution_slices[i+1,:] = np.concatenate((u, v))
     return solution_slices
 
 def sampleNoiseInitial():
@@ -69,7 +65,7 @@ def sampleCosinePerturbations():
     params = {'delta': delta, 'eps': eps, 'a0': a0, 'a1': a1}
 
     # Simulation parameters
-    T = 100.0
+    T = 10.0
     N = 200
     L = 20.0
     dt = 1.e-3
@@ -92,7 +88,7 @@ def sampleCosinePerturbations():
     rng = rd.RandomState(seed=100)
     min_n = 0
     max_n = N // 2
-    max_delta = 0.01
+    max_delta = 0.1
     n_initials = 1000
     for j in range(n_initials):
         print('initial ', j)
@@ -107,5 +103,16 @@ def sampleCosinePerturbations():
         # Store the time evolution
         np.save('./../data/singleparameter/FHN_SingleEpsilon_SinePerturbationEvolution_Initial=' + str(j) + '_eps=' + str(eps).replace('.', 'p') + '_dT=' + str(dt).replace('.', 'p') + '.npy', evolution)
 
+def plotTransients():
+    n_initials = 1000
+    for j in range(n_initials):
+        print('initial ', j)
+        evolution = np.load('./../data/singleparameter/FHN_SingleEpsilon_SinePerturbationEvolution_Initial=' + str(j) + '_eps=0p1_dT=0p001.npy')
+        norm_evolution = lg.norm(evolution, axis=1)
+        plt.plot(norm_evolution, color='tab:blue')
+
+    plt.show()
+
 if __name__ == '__main__':
     sampleCosinePerturbations()
+    plotTransients()
