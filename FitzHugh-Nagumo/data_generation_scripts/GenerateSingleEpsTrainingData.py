cf41ca84bb026d2be7c311196b1432de34715ea2
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-01-18 08:30:11 -0500

    Removing code for cosine perturbations.

--- FitzHugh-Nagumo/data_generation_scripts/GenerateSingleEpsTrainingData.py
+++ FitzHugh-Nagumo/data_generation_scripts/GenerateSingleEpsTrainingData.py
@@ -71,62 +71,6 @@ def sampleEigenPerturbation():
         # Store the time evolution
         np.save('./../data/singleparameter/FHN_SingleEpsilon_POD_Initial=' + str(j) + '_eps=' + str(eps).replace('.', 'p') + '_dT=' + str(dt).replace('.', 'p') + '.npy', evolution)
 
-def sampleCosinePerturbations():
-    # Model parameters
-    eps = 0.1
-    a0 = -0.03
-    a1 = 2.0
-    delta = 4.0
-    params = {'delta': delta, 'eps': eps, 'a0': a0, 'a1': a1}
-
-    # Simulation parameters
-    T = 100.0
-    N = 200
-    L = 20.0
-    dt = 1.e-3
-    dT = 100 * dt
-    dx = L / N
-
-    # Calculate the steady state by timestepping, followed by Newton-Krylov
-    T_psi = 1.0
-    x_array = np.linspace(0.0, L, N)
-    u0 = sigmoid(x_array, 6.0, -1, 1.0, 2.0)
-    v0 = sigmoid(x_array, 10, 0.0, 2.0, 0.1)
-    u0, v0 = fhn_euler_timestepper(u0, v0, dx, dt, 100.0, params, verbose=False)
-    x0 = np.concatenate((u0, v0))
-    x_ss = calculateSteadyState(x0, T_psi, dx, dt, params)
-    u_ss = x_ss[0:N]
-    v_ss = x_ss[N:]
-    print('Final psi :', lg.norm(psi(x_ss, T_psi, dx, dt, params)))
-    
-    rng = rd.RandomState(seed=100)
-    min_n = 0
-    max_n = N // 2
-    max_delta = 0.1
-    n_initials = 1000
-    for j in range(n_initials):
-        print('Initial #', j+1)
-        delta_u = max_delta * rng.uniform(-1.0, 1.0)
-        delta_v = max_delta * rng.uniform(-1.0, 1.0)
-        n_u = rng.randint(min_n, max_n+1)
-        n_v = rng.randint(min_n, max_n+1)
-        u = u_ss + delta_u * np.cos(2.0 * np.pi * n_u * x_array / L)
-        v = v_ss + delta_v * np.cos(2.0 * np.pi * n_v * x_array / L)
-        evolution = timeSimulation(u, v, dx, dt, T, dT, params)
-
-        # Store the time evolution
-        np.save('./../data/singleparameter/FHN_SingleEpsilon_SinePerturbationEvolution_Initial=' + str(j) + '_eps=' + str(eps).replace('.', 'p') + '_dT=' + str(dt).replace('.', 'p') + '.npy', evolution)
-
-def plotTransients():
-    n_initials = 1000
-    for j in range(n_initials):
-        evolution = np.load('./../data/singleparameter/FHN_SingleEpsilon_SinePerturbationEvolution_Initial=' + str(j) + '_eps=0p1_dT=0p001.npy')
-        norm_evolution = lg.norm(evolution, axis=1)
-        plt.plot(norm_evolution, color='tab:blue')
-    plt.xlabel(r'$t$ [s/10]')
-    plt.ylabel('Transient Norms')
-    plt.legend([r'$\sqrt{\|u(t)\|^2 + \|v(t)\|^2}$'])
-    plt.show()
 
 if __name__ == '__main__':
     sampleEigenPerturbation()
