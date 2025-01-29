ddf394be7e07238804adf20007510991b8c4e4c1
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-01-29 16:04:31 -0500

    wrote second-branchg deeponet for multi-parameter FHN

--- FitzHugh-Nagumo/deeponet_multiparameter/DeepONet.py
+++ FitzHugh-Nagumo/deeponet_multiparameter/DeepONet.py
@@ -5,18 +5,19 @@ from collections import OrderedDict
 
 # Class for Dense Neural Networks used for branch and trunk networks.
 class DenseNN(nn.Module):
-    def __init__(self, layers=[]):
+    def __init__(self, layers=[], dtype=pt.float32):
         super(DenseNN, self).__init__()
+        print('dtype', dtype)
         
         # Create all feed-forward layers
         self.depth = len(layers) - 1
-        self.activation = nn.Tanh
+        self.activation = nn.Tanh()
 
         layer_list = list()
         for i in range(self.depth - 1):
-            layer_list.append(('layer_%d' % i, pt.nn.Linear(layers[i], layers[i+1])))
-            layer_list.append(('activation_%d' % i, self.activation()))
-        layer_list.append(('layer_%d' % (self.depth-1), pt.nn.Linear(layers[-2], layers[-1])))
+            layer_list.append(('layer_%d' % i, pt.nn.Linear(layers[i], layers[i+1], dtype=dtype)))
+            layer_list.append(('activation_%d' % i, self.activation))
+        layer_list.append(('layer_%d' % (self.depth-1), pt.nn.Linear(layers[-2], layers[-1], dtype=dtype)))
         layerDict = OrderedDict(layer_list)
 
         # Combine all layers in a single Sequential object to keep track of parameter count
@@ -27,17 +28,19 @@ class DenseNN(nn.Module):
     
 # Class for general DeepONets
 class DeepONet(nn.Module):
-    def __init__(self, branch_layers, trunk_layers):
+    def __init__(self, eps_branch_layers, branch_layers, trunk_layers, dtype=pt.float32):
         super(DeepONet, self).__init__()
 
         self.branch_input_size = branch_layers[0]
         self.p = branch_layers[-1] // 2
         assert trunk_layers[-1] == 2 * self.p
 
-        self.branch_net = DenseNN(branch_layers)
-        self.trunk_net = DenseNN(trunk_layers)
+        self.eps_branch_net = DenseNN(eps_branch_layers, dtype=dtype)
+        self.branch_net = DenseNN(branch_layers, dtype=dtype)
+        self.trunk_net = DenseNN(trunk_layers, dtype=dtype)
         
         self.params = []
+        self.params.extend(self.eps_branch_net.parameters())
         self.params.extend(self.branch_net.parameters())
         self.params.extend(self.trunk_net.parameters())
         print('Number of DeepONet Parameters:', sum(p.numel() for p in self.parameters()))
@@ -47,12 +50,14 @@ class DeepONet(nn.Module):
 
     # The input data x = array([branch_x, trunk_x]) with shape (batch_size, self.branch_input_size)
     def forward(self, x):
-        branch_x = x[:, 0:self.branch_input_size]
-        trunk_x = x[:, self.branch_input_size:]
+        branch_eps_x = x[:, 0:1]
+        branch_x = x[:, 1:self.branch_input_size+1]
+        trunk_x = x[:, self.branch_input_size+1:]
+        eps_branch_output = self.eps_branch_net.forward(branch_eps_x)
         branch_output = self.branch_net.forward(branch_x)
         trunk_output = self.trunk_net.forward(trunk_x)
      
         # Multiply element-wise and sum over axis=1 (p axis)
-        u_output = pt.sum(branch_output[:,0:self.p] * trunk_output[:,0:self.p], dim=1)
-        v_output = pt.sum(branch_output[:,self.p:] * trunk_output[:,self.p:], dim=1)
+        u_output = pt.sum(eps_branch_output[:,:self.p] * branch_output[:,:self.p] * trunk_output[:,:self.p], dim=1)
+        v_output = pt.sum(eps_branch_output[:,self.p:] * branch_output[:,self.p:] * trunk_output[:,self.p:], dim=1)
         return pt.hstack((u_output[:,None], v_output[:,None]))
