260bf59c5854bb0ea794654f6f2db1be51950bf4
Gianluca Fabiani <fabianigianluca94@gmail.com>
2025-06-19 00:08:59 +0200

    Add files via upload

---
+++ FitzHugh_Nagumo_simulated_with_LBM/RandONets_matlab_codes/task1_train_RandONets_FHN/main_RandDeepOnet_FHN_LBM_2outputs_also_single_parameter.m
@@ -0,0 +1,264 @@
+clc
+clear
+close all
+set(0,'DefaultLineLineWidth',2)
+%
+%load('data_LBM_FHN_longer.mat') %dt=1
+load('data_LBM_FHN_dt01.mat') %dt=0.1
+%load('data_LBM_FHN_shorter.mat') %dt=0.1
+%load('data_LBM_FHN_shorter2.mat') %dt=0.01
+rng(5)
+save_on=1;
+%
+ep0=min(PARAM_train_branch);
+epf=max(PARAM_train_branch);
+gpu_flag=1; %training with GPUs
+parametric=0; %parametric DeepONet
+identity=0;
+% select if you want (1) also JL RandOnet or not (2)
+%select how many epsilon you want
+best_RandONet=[];
+best_err=1e10;
+best_knb=1;
+flag_single=0;
+simmetry=0;
+%simmetry=1;
+%
+val_epsilon=PARAM_test_branch(2000); %0.1 or 1 (is effectively 3.8 max)  <--------------------
+% %val_epsilon=1;
+% %
+if val_epsilon<1
+    parametric=0;
+end
+% if parametric==1
+%     val_epsilon=1; %cannot do parametric with parameter fixed
+% end
+if val_epsilon<1
+    I1=PARAM_train_branch==val_epsilon;
+    I2=PARAM_test_branch==val_epsilon;
+    flag_single=1;
+else
+    flag_single=0;
+    I1=UV0train_branch(end,:)<val_epsilon;
+    I2=UV0test_branch(end,:)<val_epsilon;
+end
+ns=sum(I1);
+ff_train=UV0_train_branch(:,I1);
+param_train=PARAM_train_branch(:,I1);
+gg_train_out=[U1_train_out(:,I1);V1_train_out(:,I1)];
+%
+ff_test=UV0_test_branch(:,I2);
+param_test=PARAM_test_branch(:,I2);
+gg_test_out=[U1_test_out(:,I2);V1_test_out(:,I2)];
+%
+ff_val=ff_test;
+param_val=param_test;
+gg_val_out=gg_test_out;
+
+
+ff_tot=UV0_train_branch;
+%
+pod_modes=1;
+if pod_modes==1
+bias_proj=zeros(82,1); %mean(ff_tot,2);
+%Cov=1/(size(ff_val,2)-1)*(ff_val-bias_proj)*(ff_val-bias_proj)';
+Cov=1/(size(ff_tot,2)-1)*(ff_tot*ff_tot');
+[Vkov,Dkov]=eig(Cov);
+Dkov=max(real(diag(Dkov)),0);
+[Dkov,Iord]=sort(Dkov,1,"descend");
+Vkov=Vkov(:,Iord);
+tol=0.9999;
+tempkov=0;
+Skov=sum(Dkov);
+kcov=0;
+while tol>tempkov
+    kcov=kcov+1;
+    tempkov=tempkov+Dkov(kcov)/Skov;
+end
+Vkov_proj=Vkov(:,1:kcov)';
+save('PODmodes3_single.mat','Vkov_proj');
+%
+ff_train=Vkov_proj*(ff_train-bias_proj);
+ff_test=Vkov_proj*(ff_test-bias_proj);
+ff_val=Vkov_proj*(ff_val-bias_proj);
+%
+%gg_test_out=Vkov_proj*gg_test_out;
+%gg_train_out=Vkov_proj*gg_train_out;
+%gg_val_out=Vkov_proj*gg_val_out;
+end
+%
+dt=dtRON;
+Ny=length(Ytrunk); %51; 
+%
+Nt=Ny*3; %300; %number of neurons in the trunk hidden layer
+NNb=[10,20,40,80,100,150,300,500,1000];%1000,1500,2000];%,1500,2000]; %number of neurons in the branch hidden layer
+kmodel=2;
+%data_case='fews'; iters=10;
+data_case='many'; iters=3; %Iterations are used solely to improve computational time estimation.
+n_out=2;
+%
+variation_on=0;
+if variation_on==1
+    gg_train_out=gg_train_out-ff_train;
+    gg_test_out=gg_test_out-ff_test;
+    gg_val_out=gg_val_out-ff_val;
+end
+%
+%
+knb=0;
+for Nb=NNb
+    knb=knb+1;
+    knb/length(NNb)
+    pause(0.0001)
+    timetime=0;
+    for it=1:iters
+        err_iter=1e10;
+        tstart=tic;
+        %RandONet=train_RandONet_parametric(ff_train,param_train,Ytrunk,gg_train_out,Nt,Nb,kmodel,gpu_flag,n_out,identity);
+        RandONet=train_RandONet(ff_train,Ytrunk,gg_train_out,Nt,Nb,kmodel,gpu_flag,n_out);
+        %
+        timetime=timetime+toc(tstart);
+        %
+        %validation/ (or train/test set) (to select) (should be validation))
+        Gnet_val=EVAL_flags_RandONet(RandONet,ff_val,param_val,Ytrunk,parametric);
+        %
+        %err_temp=max(max(abs(gg_val_out-Gnet_val)));
+        err_temp=prctile(sqrt(sum((gg_val_out-Gnet_val).^2)),95,2); %sqrt(sum((gg_val_out-Gnet_val).^2));
+        if err_iter>err_temp
+            save_iter_RandONet=RandONet;
+            err_iter=err_temp;
+        end
+            %
+    end %(end iters)
+    timetime=timetime/iters;
+    RandONet=save_iter_RandONet;
+    %
+    %test set
+    Gnet_test=EVAL_flags_RandONet(RandONet,ff_test,param_test,Ytrunk,parametric);
+    errMSE_RFFN_test(knb)=mean(mean((gg_test_out-Gnet_test).^2));
+    errmax_RFFN_test(knb)=max(max(abs(gg_test_out-Gnet_test)));
+    errL2_RFFN_test(knb,:)=sqrt(sum((gg_test_out-Gnet_test).^2));
+    if best_err>prctile(errL2_RFFN_test(knb,:),95,2) %errmax_RFFN_test(knb)
+        best_err=prctile(errL2_RFFN_test(knb,:),95,2);
+        best_RandONet=RandONet;
+        best_knb=knb;
+    end
+    time_RFFN(knb)=timetime;
+end
+%
+errmL2_RFFN_test=median(errL2_RFFN_test,2)';
+err_95L2_RFFN_test=prctile(errL2_RFFN_test,95,2)';
+err_05L2_RFFN_test=prctile(errL2_RFFN_test,5,2)';
+
+
+figure(1)
+hold off
+fill([NNb,fliplr(NNb)],[err_05L2_RFFN_test,fliplr(err_95L2_RFFN_test)],...
+    'm','FaceAlpha',0.3,'LineStyle','none')
+hold 
+set(gca,'Xscale','log','Yscale','log')
+loglog(NNb,errmL2_RFFN_test,'x-r','MarkerSize',8)
+loglog(NNb,errMSE_RFFN_test,'x--c','MarkerSize',8)
+loglog(NNb,errmax_RFFN_test,'x:b','MarkerSize',8)
+%loglog(NNb(best_knb),errmax_RFFN_test(best_knb),'s-g','MarkerSize',10)
+loglog(NNb(best_knb),errmL2_RFFN_test(best_knb),'s-g','MarkerSize',10)
+grid on
+set(gca,'FontSize',18,'YTick',10.^(-32:2:32))
+xlabel('$M$ (branch neurons)','Interpreter','latex')
+ylabel('error')
+legend('RFFN 5\%-95\% $L^2$','RFFN median $L^2$','RFFN MSE',...
+    'RFFN MaxAE','interpreter','latex','NumColumns',2,'FontSize',14)
+% legend('JL max-$L^2$','JL MSE','JL MaxAE',...
+%     'RFFN max-$L^2$','RFFN MSE','RFFN MaxAE','interpreter','latex',...
+%     'NumColumns',2,'FontSize',14)
+VV=axis;
+ylim(10.^[log10(VV(3))*1.1,log10(VV(4))+(log10(VV(4))-log10(VV(3)))/4])
+%
+
+%
+figure(2)
+hold off
+fill([time_RFFN,fliplr(time_RFFN)],[err_05L2_RFFN_test,fliplr(err_95L2_RFFN_test)],...
+    'm','FaceAlpha',0.3,'LineStyle','none')
+hold on
+set(gca,'Xscale','log','Yscale','log')
+loglog(time_RFFN,errmL2_RFFN_test,'x-r','MarkerSize',8)
+loglog(time_RFFN,errMSE_RFFN_test,'x--c','MarkerSize',8)
+loglog(time_RFFN,errmax_RFFN_test,'x:b','MarkerSize',8)
+%loglog(time_RFFN(best_knb),errmax_RFFN_test(best_knb),'s-g','MarkerSize',10)
+loglog(time_RFFN(best_knb),errmL2_RFFN_test(best_knb),'s-g','MarkerSize',10)
+grid on
+set(gca,'FontSize',18,'YTick',10.^(-32:2:32))
+xlabel('time [seconds]','Interpreter','latex')
+ylabel('error')
+    legend('RFFN 5\%-95\% $L^2$','RFFN median $L^2$','RFFN MSE',...
+    'RFFN MaxAE','interpreter','latex','NumColumns',2,'FontSize',14)
+%VV=axis;
+ylim(10.^[log10(VV(3))*1.1,log10(VV(4))+(log10(VV(4))-log10(VV(3)))/4])
+%
+%
+
+%
+tot_ele=size(ff_test,2);
+
+for i=1:9
+    k=round((i-1)/9*tot_ele+1);%plot(repmat(Ytrunk,n_out,1),
+    Gnetk=EVAL_flags_RandONet(best_RandONet,ff_test(:,k),param_test(k),Ytrunk,parametric);
+    figure(3)
+    subplot(3,3,i)
+    hold off
+    for ki=1:n_out
+    plot(Ytrunk,gg_test_out((1:Ny)+Ny*(ki-1),k))
+    hold on
+    plot(Ytrunk,Gnetk((1:Ny)+Ny*(ki-1)),'--')
+    end
+    
+    figure(4)
+    subplot(3,3,i)
+    hold off
+    for ki=1:n_out
+    semilogy(Ytrunk,abs(gg_test_out((1:Ny)+Ny*(ki-1),k)-Gnetk((1:Ny)+Ny*(ki-1)) ))
+    hold on
+    %plot(repmat(Ytrunk,n_out,1),abs(gg_test_out(:,k)-Gnetk))
+    end
+end
+folder='figures/';
+if parametric==1
+fig_base='fig_FHN_0005_0995_multi_par_RandONet_';
+else
+fig_base='fig_FHN_010_single_par_RandONet_';
+end
+fig_name={'for_neurons','for_time','some_sols','some_errs'};
+if save_on==1
+    for i=1:4
+        filename = fullfile(folder, [fig_base,fig_name{i}]);
+        figure(i)
+        pause(0.001)
+        fig=gcf;
+        % Save as .fig
+        savefig(fig, [filename, '.fig']);
+        % Save as .eps
+        saveas(fig, [filename, '.eps'], 'epsc');
+        % Save as .pdf
+        saveas(fig, [filename, '.pdf'], 'pdf');
+        % Save as .jpg
+        saveas(fig, [filename, '.jpg'], 'jpg');
+    end
+end
+
+%
+RandONet=best_RandONet;
+if save_on==1
+if parametric==1
+    if pod_modes==1
+        save('POD_RandONet_parametric_FHN_0005_0995','RandONet','flag_single','parametric','simmetry')
+    else
+        save('RandONet_parametric_FHN_0005_0995','RandONet','flag_single','parametric','simmetry')
+    end
+else
+    save('POD_RandONet_single_FHN_010','RandONet','flag_single','parametric','simmetry')
+end
+if pod_modes==1
+    save('POD3modes_single','Vkov_proj','bias_proj')
+end
+end
---
+++ FitzHugh_Nagumo_simulated_with_LBM/RandONets_matlab_codes/task1_train_RandONets_FHN/train_RandONet.m
@@ -0,0 +1,183 @@
+% train_RandONet trains a Random Projection-based Operator Network (RandONet) model.
+%
+% Syntax: net = train_RandONet(ff, yy, Nt, Nb, kmodel)
+%
+% Inputs:
+%   - ff      : Input matrix (functions) for the branch network.
+%   - yy      : Input vector (spatial locations) for the trunk network.
+%   - G       : Input matrix (transformed functions G[f](y)
+%   - Nt      : Number of neurons in the trunk network (default: 200).
+%   - Nb      : Number of neurons in the branch network (default: 1000).
+%   - kmodel  : Model type (1 for JL, 2 for RFFN; default: 2).
+%
+% Output:
+%   - net     : Trained RandONet model containing fields for the trunk and
+%               branch networks, including weights and biases.
+%
+%   - net : Structure containing the parameters of the RandONet model.
+%           Fields include:
+%             - tr_fT : Trunk network activation function (nonlinear transformation).
+%             - tr_fB : Branch network activation function (nonlinear transformation).
+%             - alphat, betat : Parameters for input transformation in the trunk network.
+%             - alphab, betab : Parameters for input transformation in the branch network.
+%             - C : Weight matrix for the inner product.
+%
+% The function initializes network parameters, trains using COD-based pseudo-inverse 
+% of the trunk and branch layers, and stores the results in the output net.
+%
+% DISCLAIMER: This software is provided "as is" without warranty of any kind.
+% This includes, but is not limited to, warranties of merchantability,
+% fitness for a particular purpose, and non-infringement.
+% The authors and copyright holders are not liable for any claims, damages,
+% or other liabilities arising from the use of this software.
+%
+%Copyright (c) 2024 Gianluca Fabiani
+%
+%Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+% You may not use this material for commercial purposes.
+% If you remix, transform, or build upon this material,
+% you must distribute your contributions under the same license as the original.
+
+function net=train_RandONet(ff,yy,G,Nt,Nb,kmodel,gpu_flag,n_out)
+%check arguments %basic selection
+Ny=size(yy,1); %number of points of the evaluation grid
+Nyg=size(G,1);
+if nargin<4
+    Nt=200;
+end
+if nargin<5
+    Nb=1000;
+end
+if nargin<6
+    kmodel=2; %RFFN
+end
+if nargin <7
+    gpu_flag=1;
+end
+if nargin<8
+    n_out=Nyg/Ny;
+end
+if isempty(Nt)
+    Nt=200;
+end
+if isempty(Nb)
+    Nb=1000;
+end
+if isempty(kmodel)
+    kmodel=2;
+end
+if isempty(gpu_flag)
+    gpu_flag=1;
+end
+%
+%check
+if n_out~=Nyg/Ny
+    disp('Error in number of outputs')
+    return
+end
+%
+%main part of training
+tr_fT=@(x) tanh(x); %activation function of the trunk network (only this one tested)
+tr_fT_name='tanh';
+if kmodel==1
+    tr_fB=@(x) x; flag_branch=1; %%%%%%%JL based model
+    tr_fB_name='lin';
+elseif kmodel==2
+    tr_fB=@(x) cos(x); flag_branch=0;  %%%%%%RFFN based model
+    tr_fB_name='cos';
+end
+Nx=size(ff,1); %number of discretization points of functions
+Ns=size(ff,2);
+%
+x0=min(yy); xf=max(yy); %spatial interval
+DX=xf-x0; %size of the interval
+alphat=2*(4+Nt*1/5)*(2*rand(1,Nt)-1)/DX; %random internal weights of the trunk
+cent=linspace(x0,xf,Nt);
+betat=-alphat.*cent; % random biases of the trunk
+Tr=tr_fT(yy*alphat+betat); %trunk hidden layer
+%branch net
+U0=min(min(ff,[],2));
+Uf=max(max(ff,[],2));
+DU=Uf-U0; %normalization of the function space
+%U0=U0-DU/10;
+%Uf=Uf+DU/10;
+if flag_branch==1 %JL
+    alphab=sqrt(1/Nb)*randn(Nb,Nx)/DU; %JL random internal weighrs in the branch
+    betab=zeros(Nb,1);
+end
+if flag_branch==0 %RFFN
+    alphab=2*Nb^(1/8)*sqrt(2/Nb)*randn(Nb,Nx)/DU.*rand(Nb,Nx); %RFFN random internal weighrs in the branch
+    betab=rand(Nb,1)*2*pi;
+end
+%         if flag_branch==2 (not tested)
+%             alphab=1/Nx*(4+Nb.^(1/Nx)*9/100)*(2*rand(Nb,Nx)-1)./DU;
+%             cenb=U0+DU.*rand(Nb,Nx);
+%             betab=-sum(alphab.*cenb,2);
+%         end
+Br_train=tr_fB(alphab*ff+betab); %branch hidden layer
+
+if (Nb^2*Ns+Ns^2+Nb)<2*100^3 || gpu_flag==0
+    tic;
+    [Qr,T11,Vp]=pinvCOD(Tr); %COD based pseudo inverse of the trunk hidden layer
+    [Qr2,T112,Vp2]=pinvCOD(Br_train,[]); %COD based pseudo inverse of the branch hidden layer
+    %
+    flag_check=1;
+    toc
+else
+    flag_check=0;
+    tic;
+    %Tr=gpuArray(Tr);
+    [Qr,T11,Vp]=pinvCOD(Tr);
+    Br=gpuArray(Br_train);
+    tol=min(max(eps(norm(Br_train)*max(size(Br_train)))*1e1,1e-12),1e-5);
+    %tol=1e-8;
+    invBr=pinv(Br,tol);
+    %C=pinv(Tr,1e-8)*G*pinv(Br,1e-8);
+    % [Qr,T11,Vp]=pinvCOD(Tr); %COD based pseudo inverse of the trunk hidden layer
+    % [Qr2,T112,Vp2]=pinvCOD(Br_train,[]); %COD based pseudo inverse of the branch hidden layer
+    % C=((Vp*(T11\(Qr'*G))*Vp2)/T112)*Qr2'; %external weights
+    toc
+    disp('gpu-based computations')
+end
+if n_out==1
+    if flag_check==0
+    C=Vp*(T11\(Qr'*G))*invBr;
+    else
+    C=((Vp*(T11\(Qr'*G))*Vp2)/T112)*Qr2'; %external weights
+    end
+    if isgpuarray(C)==1
+        C=gather(C);
+    end
+elseif n_out >1
+    for jj=1:n_out
+        ind=(1:Ny)+(jj-1)*Ny;
+        if flag_check==0
+        C{jj}=Vp*(T11\(Qr'*G(ind,:)))*invBr;
+        else
+        C{jj}=((Vp*(T11\(Qr'*G(ind,:)))*Vp2)/T112)*Qr2'; %external weights
+        end
+        if isgpuarray(C{jj})==1
+            C{jj}=gather(C{jj});
+        end
+    end
+end
+%save net
+net.tr_fT=tr_fT_name;
+net.alphat=alphat;
+net.betat=betat;
+net.tr_fB=tr_fB_name;
+net.alphab=alphab;
+net.betab=betab;
+net.C=gather(C);
+net.parametric=0;
+net.n_out=n_out;
+%
+Gnet=eval_RandONet(net,ff,yy);
+% Gnet21=Tr*C{1}*Br;
+% Gnet22=Tr*C{2}*Br;
+% Gnet2=[Gnet21;Gnet22];
+Nb
+err=norm(G-Gnet)
+
+
+end
